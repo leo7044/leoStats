85e29a5ad53a759a991acf2525627554ba32a6b8
leo7044 <cc.ta.leo7044@gmail.com>
2018-04-26 12:54:58 +0200

    fixed multiple worlds (MemberRole)

--- php/manageBackend.php
+++ php/manageBackend.php
@@ -287,7 +287,7 @@ if (!$conn->connect_error)
                         (
                         IF
                             (
-                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId')<=a.MemberRole,
+                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId' AND p.WorldId=s.WorldId)<=a.MemberRole,
                                 true,
                                 p.AccountId='$OwnAccountId'
                             )
@@ -456,7 +456,7 @@ if (!$conn->connect_error)
                             AND
                             IF
                             (
-                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId')<=a.MemberRole,
+                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId' AND p.WorldId=s.WorldId)<=a.MemberRole,
                                 true,
                                 p.AccountId='$OwnAccountId'
                             )
@@ -532,7 +532,7 @@ if (!$conn->connect_error)
                             AND
                             IF
                             (
-                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId')<=a.MemberRole,
+                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId' AND p.WorldId=s.WorldId)<=a.MemberRole,
                                 true,
                                 p.AccountId='$OwnAccountId'
                             )
@@ -598,7 +598,7 @@ if (!$conn->connect_error)
                             AND
                             IF
                             (
-                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId')<=a.MemberRole,
+                                (SELECT p.MemberRole FROM relation_player p WHERE p.AccountId='$OwnAccountId' AND p.WorldId=s.WorldId)<=a.MemberRole,
                                 true,
                                 p.AccountId='$OwnAccountId'
                             )
