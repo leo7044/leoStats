79e59eb9be7fd9e55996bd94d83f8e58a896c720
leo7044 <cc.ta.leo7044@gmail.com>
2020-02-27 17:22:04 +0100

    getOverview

--- js/buildSqlForGetOverviewDefault.js
+++ js/buildSqlForGetOverviewDefault.js
@@ -1,9 +1,13 @@
 var strQuery = 'SELECT ';
-for (var i = 0; i <= 81; i++)
+var maxLevel = 82;
+for (var i = 0; i <= maxLevel; i++)
 {
+    strQuery += 'SUM(CASE WHEN ba.LvLBase BETWEEN ' + i + ' AND ' + i + '.99 THEN 1 ELSE 0 END) AS LvLBase' + i + ',\n';
     strQuery += 'SUM(CASE WHEN ba.LvLOff BETWEEN ' + i + ' AND ' + i + '.99 THEN 1 ELSE 0 END) AS LvLOff' + i + ',\n';
+    strQuery += 'SUM(CASE WHEN ba.LvLDef BETWEEN ' + i + ' AND ' + i + '.99 THEN 1 ELSE 0 END) AS LvLDef' + i + ',\n';
+    strQuery += 'SUM(CASE WHEN ba.LvLSup=' + i + ' THEN 1 ELSE 0 END) AS LvLSup' + i + ',\n';
 }
-strQuery += 'SUM(CASE WHEN ba.LvLOff BETWEEN 82 AND 82.99 THEN 1 ELSE 0 END) AS LvLOff82\n';
+strQuery = strQuery.substr(0, strQuery.length - 2) + '\n';
 strQuery += 'FROM relation_bases b JOIN relation_alliance a ON a.WorldId=b.WorldId\n';
 strQuery += 'JOIN relation_player p ON p.WorldId=b.WorldId and p.AllianceId=a.AllianceId AND p.AccountId=b.AccountId\n';
 strQuery += 'JOIN bases ba ON ba.WorldId=b.WorldId AND ba.BaseId=b.BaseId\n';
